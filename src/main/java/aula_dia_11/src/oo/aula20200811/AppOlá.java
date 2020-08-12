06fcf433403418337bf0c2cc8664a52e3473d2e3
Paulo Henrique <paulohaduken@live.com>
2020-08-12 14:09:44 -0300

    adicionado a atividade aula dia 11

---
+++ src/main/java/aula_dia_11/src/oo/aula20200811/AppOlá.java
@@ -0,0 +1,15 @@
+package oo.aula20200811;
+
+public class AppOlá {
+	
+	private static void printar() {
+		System.out.println("\nOlha, funciona!\n");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		printar();
+
+	}
+
+}
